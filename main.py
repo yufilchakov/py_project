66f2770cbdac3825b7fce707469b28a5208f3561
Yura Filchakov <filchakov1993@gmail.com>
2024-03-24 00:20:38 +0700

    import abb_numbers

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from funcs import add_numbers
+#from funcs import add_numbers
+from funcs import abb_numbers
 
-a = 12
-b = 17
-result = add_numbers(a, b)
-print(f"{a} + {b} = {result}")
+#a = 12
+#b = 17
+#result = add_numbers(a, b)
+#print(f"{a} + {b} = {result}")
+
+a = 57
+b = 16
+result = abb_numbers(a, b)
+print(f"{a} - {b} = {result}")
